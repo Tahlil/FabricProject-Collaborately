2eb3341680e85d4efb18e139c8bf71ba309cb201
Tahlil <bsse0803@iit.du.ac.bd>
2020-08-13 03:28:00 +0600

    working with the default network

--- vars/create-artifacts.sh
+++ vars/create-artifacts.sh
@@ -4,7 +4,7 @@ CHANNEL_NAME="globalchannel"
 echo $CHANNEL_NAME
 
 # Generate System Genesis block
-configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
+configtxgen -profile OrdererGenesis -configPath -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
 
 # Generate channel configuration block
-configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./globalchannel.tx -channelID $CHANNEL_NAME
+configtxgen -profile OrgChannel -outputCreateChannelTx ./globalchannel.tx -channelID $CHANNEL_NAME
